08fc26242c5163b34e92efbfd2bac1475268232f
Joshua Crotts <joshuacrotts@Joshuas-MacBook-Pro-2.local>
2019-02-13 22:09:51 -0500

    Changed particle configuration

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 100,
+        "value": 200,
         "density": {
           "enable": true,
           "value_area": 800
@@ -51,7 +51,7 @@ particlesJS('particles-js',
         }
       },
       "size": {
-        "value": 5,
+        "value": 1,
         "random": true,
         "anim": {
           "enable": false,

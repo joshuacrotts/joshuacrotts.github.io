4c179c0daaa8e899dcd1e2809f4b39f635e38afb
JoshuaCrotts <joshuacrotts@yahoo.com>
2019-02-14 10:52:49 -0500

    Changed particle effects

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 200,
+        "value": 80,
         "density": {
           "enable": true,
           "value_area": 800

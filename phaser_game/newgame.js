96eb9dd9cd3cf85f2140ced7de9bb2eecff62e8d
Viola Munos <vcmunos@uncg.edu>
2019-04-24 16:02:53 -0400

    Added proper collision between the ball and bricks

--- phaser_game/newgame.js
+++ phaser_game/newgame.js
@@ -19,6 +19,9 @@ let score = 0
 const ROW_BRICKS = 5
 const COL_BRICKS = 7
 
+const BRICK_WIDTH = 60
+const BRICK_HEIGHT = 15
+
 function preload()
 {
   game.load.image('sky', 'assets/sky.png')
@@ -47,26 +50,36 @@ function create()
   balls = game.add.group()
   balls.enableBody = true
 
+  /*Initializes the bricks (position and type)*/
+  let leftOffset = 150
   //Offset from the far left of the screen (50)
-  for(var y = 20; y <= COL_BRICKS * 50; y += 20)
+  for(var y = 30; y <= COL_BRICKS * 30; y += 30)
   {
-    for(var x = 30; x < game.world.height; x += 70)
+    for(var x = leftOffset; x < game.world.height; x += BRICK_WIDTH + 10)
     {
+      let brickType = game.rnd.integerInRange(0, 3)
+      let brick;
 
+      switch(brickType)
+      {
+        case 0: brick = bricks.create(x, y, 'redbrick'); break
+        case 1: brick = bricks.create(x, y, 'bluebrick'); break
+        case 2: brick = bricks.create(x, y, 'greenbrick'); break
+        case 3: brick = bricks.create(x, y, 'yellowbrick'); break
+      }
+      brick.body.immovable = true
     }
   }
-  let brick = platforms.create(400, 450, 'redbrick')
-  brick.body.immovable = true
 
+  //Initializes a single ball with a fixed position
   let ball = balls.create(300, 300, 'ball')
 
-  player = game.add.sprite(32, game.world.height - 150, 'paddle')
+  //Adds the paddle to the screen
+  player = game.add.sprite(32, game.world.height - 20, 'paddle')
   game.physics.arcade.enable(player)
   player.body.immovable = true
   player.body.collideWorldBounds = true
   player.anchor.set(0.5,1);
-
-
 
   scoreText = game.add.text(16, 16, '', { fontSize: '32px', fill: '#000'})
   cursors = game.input.keyboard.createCursorKeys()
@@ -79,21 +92,28 @@ function create()
     balls.children[c].body.bounce.set(1)
 
   }
+}
 
 function update()
 {
   game.physics.arcade.collide(player, balls)
-  //game.physics.arcade.collide(balls, bricks)
-  //game.physics.arcade.overlap(player, diamonds, collectDiamond, null, this)
-
+  game.physics.arcade.collide(balls, bricks, ballHitsBrick)
 
   if(cursors.left.isDown)
   {
     player.body.velocity.x = -150
-  }else if(cursors.right.isDown)
+  }
+  else if(cursors.right.isDown)
   {
     player.body.velocity.x = 150
-  }else {
+  }
+  else
+  {
     player.body.velocity.x = 0
   }
+}
+
+function ballHitsBrick(ball, brick)
+{
+  brick.kill()
 }
